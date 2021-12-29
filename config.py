002d7df6a449fc67f08fd7dff8c692ef3df2c794
itssimple1 <91212696+itssimple1@users.noreply.github.com>
2021-12-29 23:07:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ class Config:
         LOG_GROUP=int(LOG_GROUP)
     else:
         LOG_GROUP=None
-    STREAM_URL=os.environ.get("STREAM_URL", "https://radioindia.net/radio/hungamanow/icecast.audio")
+    STREAM_URL=os.environ.get("STREAM_URL", "http://103.16.47.70:9222/;")
     API_HASH = os.environ.get("API_HASH", "")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
     HELL_SESSION = os.environ.get("HELL_SESSION", "")
